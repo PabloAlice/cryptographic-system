a21eab0aecefe71e41ed10a55b5e4e72583b68af
Pablo Alice <pabloalice95@gmail.com>
2018-11-11 16:11:07 -0300

    update server to handle different methods

--- main.go
+++ main.go
@@ -1,62 +1,125 @@
 package main
 
 import (
+	"bytes"
+	"crypto/cipher"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/PabloAlice/cryptographic-system/src"
+
+	present "github.com/PabloAlice/cryptographic-system/src/presentCipher"
 	"github.com/gin-gonic/gin"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	println(r)
+func parseForm(c *gin.Context, decryption bool) (*present.Cipher, []byte, []byte, string, []byte, string, []byte) {
+	file, _ := c.FormFile("file")
+	method := c.PostForm("method")
+	rawKey := c.PostForm("key")
+	rawIv := c.PostForm("iv")
+	key := []byte(rawKey)
+	block, err := present.New(key)
+	if err != nil {
+		panic(err)
+	}
+	blockSize64 := int64(block.BlockSize())
+	src := make([]byte, file.Size+blockSize64-file.Size%blockSize64)
+	cipherData := make([]byte, len(src)+block.BlockSize())
+	cipherDataBuffer := new(bytes.Buffer)
+	cipherDataBuffer.Write(([]byte(rawIv))[:block.BlockSize()])
+	iv := cipherData[:block.BlockSize()]
+	cipherDataBuffer.Read(iv)
+	encryptedData := cipherData[block.BlockSize():]
+	encryptedFileName := fmt.Sprintf("ENC%v", file.Filename)
+	if decryption {
+		encryptedFileName = fmt.Sprintf("DEC%v", file.Filename)
+	}
+	srcFile, _ := file.Open()
+	defer srcFile.Close()
+	srcFile.Read(src)
+
+	return block, encryptedData, src, encryptedFileName, cipherData, method, iv
+}
+
+func saveFile(data []byte, fileName string) {
+	ioutil.WriteFile(fmt.Sprintf("./build/static/%v", fileName), data, 0644)
+	ioutil.WriteFile(fmt.Sprintf("/Users/pabloalice/Desktop/%v", fileName), data, 0644)
+}
+
+func encrypt(block *present.Cipher, iv []byte, method string, dest []byte, src []byte) {
+	switch method {
+	case "CBC":
+		println("CBC")
+		blockMode := cipher.NewCBCEncrypter(block, iv)
+		blockMode.CryptBlocks(dest, src)
+	case "CFB":
+		println("CFB")
+		blockMode := cipher.NewCFBEncrypter(block, iv)
+		blockMode.XORKeyStream(dest, src)
+	case "OFB":
+		println("OFB")
+		blockMode := cipher.NewOFB(block, iv)
+		blockMode.XORKeyStream(dest, src)
+	default:
+		println(fmt.Sprintf("default: %v", method))
+		blockMode := ecb.NewECBEncrypter(block)
+		blockMode.CryptBlocks(dest, src)
+	}
+}
+
+func decrypt(block *present.Cipher, iv []byte, method string, dest []byte, src []byte) {
+	switch method {
+	case "CBC":
+		println("CBC")
+		blockMode := cipher.NewCBCDecrypter(block, iv)
+		blockMode.CryptBlocks(dest, src)
+	case "CFB":
+		println("CFB")
+		blockMode := cipher.NewCFBDecrypter(block, iv)
+		blockMode.XORKeyStream(dest, src)
+	case "OFB":
+		println("OFB")
+		blockMode := cipher.NewOFB(block, iv)
+		blockMode.XORKeyStream(dest, src)
+	default:
+		println(fmt.Sprintf("default: %v", method))
+		blockMode := ecb.NewECBDecrypter(block)
+		blockMode.CryptBlocks(dest, src)
+	}
 }
 
 func main() {
 	r := gin.Default()
 	r.POST("/api/encryption", func(c *gin.Context) {
-		file, _ := c.FormFile("file")
-		method := c.PostForm("method")
-		key := c.PostForm("key")
-		iv := c.PostForm("iv")
-		println(file)
-		// Upload the file to specific dst.
-		c.SaveUploadedFile(file, fmt.Sprintf("./uploads/%s", file.Filename))
-
-		// ! TODO encrypt file
-
+		block, encryptedData, src, encryptedFileName, cipherData, method, iv := parseForm(c, false)
+		// end data parsed
+		encrypt(block, iv, method, encryptedData, src)
+		saveFile(cipherData[block.BlockSize():], encryptedFileName)
 		c.JSON(http.StatusOK, gin.H{
-			"fileName": file.Filename,
+			"fileName": encryptedFileName,
 			"method":   method,
-			"key":      key,
-			"iv":       iv,
 		})
 	})
 	r.POST("/api/decryption", func(c *gin.Context) {
-		file, _ := c.FormFile("file")
-		method := c.PostForm("method")
-		key := c.PostForm("key")
-		iv := c.PostForm("iv")
-
-		// Upload the file to specific dst.
-		c.SaveUploadedFile(file, fmt.Sprintf("./uploads/%s", file.Filename))
-
-		// ! TODO decrypt file
-
+		block, encryptedData, src, encryptedFileName, cipherData, method, iv := parseForm(c, true)
+		decrypt(block, iv, method, encryptedData, src)
+		saveFile(cipherData[block.BlockSize():], encryptedFileName)
 		c.JSON(http.StatusOK, gin.H{
-			"fileName": file.Filename,
+			"fileName": encryptedFileName,
 			"method":   method,
-			"key":      key,
-			"iv":       iv,
 		})
 	})
 
-	r.Static("/static", "./client/build/static")
+	r.Static("/static", "./build/static")
 
 	r.GET("/", func(c *gin.Context) {
-		c.File("./client/build/index.html")
+		c.File("./build/index.html")
 	})
 	log.Println("Listening...")
 	r.Run() // listen and serve on 0.0.0.0:8080
 
 }
+
+var _ cipher.Block = (*present.Cipher)(nil)
